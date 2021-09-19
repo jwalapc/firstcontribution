f09654e2dea37d087e3950c2bf0f3a7e5a1f1eea
Jwala <59509045+jwalapc@users.noreply.github.com>
2021-09-19 11:04:17 +0530

    Add files via upload

---
+++ scripts/contributorList.js
@@ -0,0 +1,10 @@
+let contributors = [
+    {
+        "github": "https://github.com/jhamadhav",
+        "name": "Madhav Jha"
+    },
+    {
+        "github": "https://github.com/jwalapc",
+        "name": "Jwala "
+    }
+]
