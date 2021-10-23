2b4d315118004fb8d5e97c941d04ff76cb8c934f
ADARSH SHARMA <adarshballabh.sharma11@gmail.com>
2021-10-24 00:12:16 +0530

    New Contributor added

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -55,5 +55,10 @@ let contributors = [{
     {
         "githhub":"https://github.com/markandey007",
         "name": "Markandey Sharma"
-    },               
+    },    
+    
+    {
+        "githhub":"https://github.com/adi1103sh",
+        "name": "Adarsh Sharma"
+    }
 ]
