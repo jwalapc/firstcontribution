46aaac3c1ebc2977b2f0133dcb80d2c7c0cdfc8b
Jwala <59509045+jwalapc@users.noreply.github.com>
2021-10-24 12:30:47 +0530

    Update contributorList.js

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,115 +1,5 @@
 let contributors = [{
 
-        "github": "https://github.com/jhamadhav",
-        "name": "Madhav Jha"
-    },
-    {
-        "github": "https://github.com/kiran11621",
-        "name": "Kiran Maharana"
-    },
-    {
-        "github": "https://github.com/jwalapc",
-        "name": "Jwala Chorasiya "
-    },
-    {
-        "github": "https://github.com/ShrutiMishra-2002",
-        "name": "Shruti Mishra"
-    },
-    {
-
-        "github": "https://github.com/ankurm23",
-        "name": "Ankur Mishra"
-    },
-    {
-
-        "github": "https://github.com/RadhikaTamhankar",
-        "name": "Radhika Tamhankar"
-    },
-      {
-        "github": "https://github.com/Aakashmau",
-        "name": "Aakash Maurya"
-    },
-    {
-        "github": "https://github.com/rohitg1010",
-        "name": "Rohit Gupta"
-    },
-    {
-        "github": "https://github.com/rishabhc9",
-        "name": "Rishabh Chopda"
-    },
-    {
-        "github": "https://github.com/RishabhDhawad",
-        "name": "Rishabh Dhawad"
-    },
-    {
-        "github": "https://github.com/shrejal99",
-        "name": "Shrejal Singh"
-    }
-
-
-    {
-
-        "github": "https://github.com/neha030",
-        "name": "Neha Jha"
-    },
-
-
-
-   
-
-    
-
-    {
-
-        "githhub":"https://github.com/markandey007",
-        "name": "Markandey Sharma"
-    },        
-    {
-        "github": "https://github.com/Dubeysumeet",
-        "name": "Sumeet Dubey"
-    }
-       
-
-
-    }, 
-    
-    {
-        "githhub":"https://github.com/ysasikala29",
-        "name": "Yaramakula Sasikala"
-    }, 
-
-    },   
-    
-   
-    
-    
-    {
-
-        "githhub":"https://github.com/Ronit11011",
-        "name": "Ronit Bhatt"
-    },               
-
-        "githhub":"https://github.com/meetshahk",
-        "name": "Meet Shah"
-    },  
-
-        "githhub":"https://github.com/markandey007",
-        "name": "Rahil Siddique"
-    },   
-        "github": "https://github.com/komaldewnani",
-        "name": "Komal Dewnani"
-    },
-            
-    {
-        "github": "https://github.com/artiyadav28",
-        "name": "Arti Yadav"        
-    }
-       
-
-
-
-
-
     "github": "https://github.com/jhamadhav",
     "name": "Madhav Jha"
 },
@@ -232,8 +122,6 @@ let contributors = [{
 {
     "github": "https://github.com/artiyadav28",
     "name": "Arti Yadav"        
-},        
-
-
+},
 
 ]
