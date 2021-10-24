de2d83e5facce71873f0c2bf23603eb651eca640
Komal Dewnani <57641789+KOMALDEWNANI@users.noreply.github.com>
2021-10-24 12:15:53 +0530

    New contributor added (#18)
    Co-authored-by: Jwala <59509045+jwalapc@users.noreply.github.com>

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,93 +1,65 @@
 let contributors = [{
 
-    "github": "https://github.com/jhamadhav",
-    "name": "Madhav Jha"
-},
-{
-    "github": "https://github.com/kiran11621",
-    "name": "Kiran Maharana"
-},
-{
-    "github": "https://github.com/jwalapc",
-    "name": "Jwala Chorasiya "
-},
-{
-    "github": "https://github.com/ShrutiMishra-2002",
-    "name": "Shruti Mishra"
-},
-{
-    "github": "https://github.com/Ankur-Kasaudhan",
-    "name": "Ankur Kasaudhan"
-},
-{
+        "github": "https://github.com/jhamadhav",
+        "name": "Madhav Jha"
+    },
+    {
+        "github": "https://github.com/kiran11621",
+        "name": "Kiran Maharana"
+    },
+    {
+        "github": "https://github.com/jwalapc",
+        "name": "Jwala Chorasiya "
+    },
+    {
+        "github": "https://github.com/ShrutiMishra-2002",
+        "name": "Shruti Mishra"
+    },
+    {
 
-    "github": "https://github.com/ankurm23",
-    "name": "Ankur Mishra"
-},
-{
+        "github": "https://github.com/ankurm23",
+        "name": "Ankur Mishra"
+    },
+    {
 
-    "github": "https://github.com/RadhikaTamhankar",
-    "name": "Radhika Tamhankar"
-},
-{
-    "github": "https://github.com/Aakashmau",
-    "name": "Aakash Maurya"
+        "github": "https://github.com/RadhikaTamhankar",
+        "name": "Radhika Tamhankar"
+    },
+      {
+        "github": "https://github.com/Aakashmau",
+        "name": "Aakash Maurya"
 
-},
-{
+    },
+    {
 
-    "github": "https://github.com/rohitg1010",
-    "name": "Rohit Gupta"
-},
-{
+        "github": "https://github.com/rohitg1010",
+        "name": "Rohit Gupta"
+    },
+    {
 
-    "github": "https://github.com/rishabhc9",
-    "name": "Rishabh Chopda"
-},
-{
-    "github": "https://github.com/RishabhDhawad",
-    "name": "Rishabh Dhawad"
-},
+        "github": "https://github.com/rishabhc9",
+        "name": "Rishabh Chopda"
+    },
+    {
+        "github": "https://github.com/RishabhDhawad",
+        "name": "Rishabh Dhawad"
+    },
 
 
-{
+    {
 
-    "github": "https://github.com/neha030",
-    "name": "Neha Jha"
-},
-
-
-{
-    "githhub": "https://github.com/markandey007",
-    "name": "Markandey Sharma"
-},
-{
-    "github": "https://github.com/prads46",
-    "name": "Pradeep Yadav"
-},
-{
-    "githhub": "https://github.com/meetshahk",
-    "name": "Meet Shah"
-},
-
-{
-    "github": "https://github.com/nitingupta-max",
-    "name": "Nitin Gupta"
-},
-
-{
-    "githhub": "https://github.com/Phantonym21",
-    "name": "Phantonym21"
-},
+        "github": "https://github.com/neha030",
+        "name": "Neha Jha"
+    },
 
 
+    
+    {
+        "github": "https://github.com/komaldewnani",
+        "name": "Komal Dewnani"
+    },
+       
 
 
-{
-    "githhub": "https://github.com/adi1103sh",
-    "name": "Adarsh Sharma"
-},
-
-
-
+    
 ]
