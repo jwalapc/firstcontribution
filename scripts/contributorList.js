f806f47c7517e34196745bfc95a743c076ff2dbc
RudraPratik30 <pratik104agrawal@gmail.com>
2021-10-24 18:34:21 +0530

    added my name

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -132,4 +132,9 @@ let contributors = [{
     "github": "https://github.com/anjali-yadav",
     "name": "Anjali Yadav"
 },
+{
+
+    "github": "https://github.com/RudraPratik30",
+    "name": "Pratik Agrawal"
+},
 ]
