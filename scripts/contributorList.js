792adac69b3dda14587a6677e9bdda6f22273f67
Maheep Kumar <61288313+MaheepK9@users.noreply.github.com>
2021-10-24 12:25:19 +0530

    Added Profile link and name in contributorList.js. (#28)
    [Maheep Kumar]
    
    Co-authored-by: Jwala <59509045+jwalapc@users.noreply.github.com>

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,5 +1,74 @@
 let contributors = [{
 
+    "github": "https://github.com/jhamadhav",
+    "name": "Madhav Jha"
+},
+
+{
+   "github": "https://github.com/MaheepK9",
+   "name": "Maheep Kumar"
+
+},
+
+{
+    "github": "https://github.com/kiran11621",
+    "name": "Kiran Maharana"
+},
+
+{
+    "github": "https://github.com/jwalapc",
+    "name": "Jwala Chorasiya "
+},
+{
+    "github": "https://github.com/ShrutiMishra-2002",
+    "name": "Shruti Mishra"
+},
+{
+    "github": "https://github.com/Ankur-Kasaudhan",
+    "name": "Ankur Kasaudhan"
+},
+{
+
+    "github": "https://github.com/ankurm23",
+    "name": "Ankur Mishra"
+},
+{
+
+    "github": "https://github.com/RadhikaTamhankar",
+    "name": "Radhika Tamhankar"
+},
+{
+    "github": "https://github.com/Aakashmau",
+    "name": "Aakash Maurya"
+
+},
+{
+
+    "github": "https://github.com/rohitg1010",
+    "name": "Rohit Gupta"
+},
+{
+
+    "github": "https://github.com/rishabhc9",
+    "name": "Rishabh Chopda"
+},
+{
+    "github": "https://github.com/RishabhDhawad",
+    "name": "Rishabh Dhawad"
+},
+
+
+{
+
+    "github": "https://github.com/neha030",
+    "name": "Neha Jha"
+},
+
+
+{
+    "githhub": "https://github.com/markandey007",
+    "name": "Markandey Sharma"
+},
         "github": "https://github.com/jhamadhav",
         "name": "Madhav Jha"
     },
@@ -25,6 +94,7 @@ let contributors = [{
         "github": "https://github.com/RadhikaTamhankar",
         "name": "Radhika Tamhankar"
     },
+
       {
         "github": "https://github.com/Aakashmau",
         "name": "Aakash Maurya"
