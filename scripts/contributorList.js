cb1e044f22ff58efa4671dc1ffabbc08e9c3fc14
Aditya <adityarajauria6@gmail.com>
2021-10-25 12:12:36 +0530

    Name added

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -8,6 +8,10 @@ let contributors = [{
     "name": "Kiran Maharana"
 },
 {
+    "github": "https://github.com/AdityaR6",
+    "name": "Aditya Rajauria"
+},
+{
     "github": "https://github.com/jwalapc",
     "name": "Jwala Chorasiya "
 },
