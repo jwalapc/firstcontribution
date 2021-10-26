0c59730a93f8184be6f10d77386b70fd1b4dd218
Vijay Kumar Prajapati <77984904+vijayyprajapati@users.noreply.github.com>
2021-10-26 10:46:09 +0530

    added name to contribution list (#45)
    Co-authored-by: Jwala <59509045+jwalapc@users.noreply.github.com>

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -1,4 +1,137 @@
+
+let contributors = [
+  {
+    github: "https://github.com/jhamadhav",
+    name: "Madhav Jha",
+  },
+  {
+    github: "https://github.com/kiran11621",
+    name: "Kiran Maharana",
+  },
+  {
+    github: "https://github.com/jwalapc",
+    name: "Jwala Chorasiya ",
+  },
+  {
+    github: "https://github.com/ShrutiMishra-2002",
+    name: "Shruti Mishra",
+  },
+  {
+    github: "https://github.com/Ankur-Kasaudhan",
+    name: "Ankur Kasaudhan",
+  },
+  {
+    github: "https://github.com/ankurm23",
+    name: "Ankur Mishra",
+  },
+  {
+    github: "https://github.com/RadhikaTamhankar",
+    name: "Radhika Tamhankar",
+  },
+  {
+    github: "https://github.com/Aakashmau",
+    name: "Aakash Maurya",
+  },
+  {
+    github: "https://github.com/rohitg1010",
+    name: "Rohit Gupta",
+  },
+  {
+    github: "https://github.com/rishabhc9",
+    name: "Rishabh Chopda",
+  },
+  {
+    github: "https://github.com/RishabhDhawad",
+    name: "Rishabh Dhawad",
+  },
+
+  {
+    github: "https://github.com/neha030",
+    name: "Neha Jha",
+  },
+
+  {
+    githhub: "https://github.com/markandey007",
+    name: "Markandey Sharma",
+  },
+  {
+    github: "https://github.com/prads46",
+    name: "Pradeep Yadav",
+  },
+  {
+    githhub: "https://github.com/meetshahk",
+    name: "Meet Shah",
+  },
+
+  {
+    github: "https://github.com/nitingupta-max",
+    name: "Nitin Gupta",
+  },
+
+  {
+    githhub: "https://github.com/Phantonym21",
+    name: "Phantonym21",
+  },
+
+  {
+    githhub: "https://github.com/adi1103sh",
+    name: "Adarsh Sharma",
+  },
+
+  {
+    github: "https://github.com/komaldewnani",
+    name: "Komal Dewnani",
+  },
+  {
+    githhub: "https://github.com/markandey007",
+    name: "Rahil Siddique",
+  },
+  {
+    github: "https://github.com/shrejal99",
+    name: "Shrejal Singh",
+  },
+  {
+    githhub: "https://github.com/Hariharan1701",
+    name: "Hariharan R",
+  },
+  {
+    githhub: "https://github.com/ysasikala29",
+    name: "Yaramakula Sasikala",
+  },
+  {
+    github: "https://github.com/Dubeysumeet",
+    name: "Sumeet Dubey",
+  },
+  {
+    githhub: "https://github.com/Ronit11011",
+    name: "Ronit Bhatt",
+  },
+  {
+    github: "https://github.com/MaheepK9",
+    name: "Maheep Kumar",
+  },
+  {
+    github: "https://github.com/artiyadav28",
+    name: "Arti Yadav",
+  },
+  {
+    github: "https://github.com/manishya1669",
+    name: "Manish Yadav",
+  },
+  {
+    github: "https://github.com/anjali-yadav",
+    name: "Anjali Yadav",
+  },
+  {
+    github: "https://github.com/vijayyprajapati",
+    name: "Vijay  Kumar Prajapati",
+  },
+];
+
+let contributors = [{
+
 let contributors = [{ 
+
 
     "github": "https://github.com/jhamadhav",
     "name": "Madhav Jha"
@@ -187,4 +320,3 @@ let contributors = [{
 
 
 
-]
