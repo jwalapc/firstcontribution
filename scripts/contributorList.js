bb1f5c4a76c157f7e5937b651040caa61e76e83c
Ujjwal Pathak <92005669+ujjwalpathaak@users.noreply.github.com>
2022-10-02 19:39:36 +0530

    Updates Contributer List

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -288,6 +288,10 @@ let contributors = [{
     "github": "https://github.com/rapid-killer-9",
     "name": "Harsh Jha"
 },   
+{
+    "github": "https://github.com/ujjwalpathaak",
+    "name": "Ujjwal Pathak"
+},   
 ]
 
 
