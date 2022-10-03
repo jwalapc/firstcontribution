236a1021689c1fb0cfb22a9923260e1844359aa7
sharma54 <91731467+sharma54@users.noreply.github.com>
2022-10-04 00:44:39 +0530

    Add files via upload

---
+++ images/userRouter.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const userCtrl = require('../controllers/userCtrl');
+const auth = require('../middleware/auth');
+
+// Register User
+router.post('/register', userCtrl.registerUser);
+
+// Login User
+router.post('/login', userCtrl.loginUser);
+
+// verify token
+router.get('/verify', userCtrl.verifiedToken);
+
+module.exports = router;
