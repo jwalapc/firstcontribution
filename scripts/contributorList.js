25acf3dca389c7cd3412bb81345391897f19ea07
pankajkr987 <94712990+pankajkr987@users.noreply.github.com>
2022-10-07 13:35:48 +0530

    Update contributorList.js
    i have my github id and also add my name

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -309,7 +309,12 @@ let contributors = [{
 
     "github": "https://github.com/sajdoi2",
     "name": "Devarsh Sajdoi"
-}, 
+},
+{   
+    "github":"https://github.com/pankajkr987",
+    "name": "Pankaj Kumar"
+},
+                    
 
 ]
 
