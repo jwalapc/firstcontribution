c24ace046c56bed8c49943c3e9f3ff3fb0e41c08
AMBAR KANSAL <75237568+Ambarcode@users.noreply.github.com>
2022-10-17 21:21:01 +0530

    Update contributorList.js

--- scripts/contributorList.js
+++ scripts/contributorList.js
@@ -314,4 +314,8 @@ let contributors = [
     github: "https://github.com/Rahulagg13",
     name: "Rahul Aggarwal",
   },
+  {
+    github: "https://github.com/Ambarcode"
+    name: "Ambar Kansal" 
+  },
 ];
